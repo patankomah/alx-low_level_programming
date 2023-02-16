a7555afee9fe6a8729c55ec6e4a3ceafd0a99b2a
patankomah <pat.ankomah11@yahoo.com>
2023-02-16 10:36:15 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Sucess)
- */
+* main - Entry point
+*
+* Return: Always 0 (Sucess)
+*/
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
