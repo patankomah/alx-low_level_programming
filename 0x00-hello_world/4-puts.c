5e36fbf8637e89259e7b4c45c8f82a458b803895
patankomah <pat.ankomah11@yahoo.com>
2023-02-16 06:17:17 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #!/bin/bash
 /**
  * main - main block
- *return: 0
+ * return: o
  */
 int main(void)
 {
-	pusts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
