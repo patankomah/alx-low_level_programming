0a2765c78c2d6c547e5d19fc7bf4d457156e16f6
patankomah <pat.ankomah11@yahoo.com>
2023-04-04 12:01:48 -0700

    Task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 
 /**
-* print_listint - prints all the elements of a linked list
-* @h: linked list of type listint_t to print
+* listint_len - return the number of elements in a linked list
+* @h: linked list of type listint_t to traverse
 *
 * Return: number of nodes
 */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 size_t num = 0;
 
 while (h)
 {
-printf("%d\n", h->n);
 num++;
 h = h->next;
 }
